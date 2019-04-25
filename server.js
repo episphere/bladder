d099741a2f7562e6d91be4d5b782be89f71fb2c4
Jonas Almeida <jalmeida@mathbiol.org>
2019-04-24 20:11:47 -0400

    testing axios

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-console.log('server started at '+Date())
 
 const http = require('http');
+const axios = require('axios');
 
 //const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  let x={hello:'world'}
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  let x = await axios.get('http://firebrowse.org/api/v1/Samples/mRNASeq?format=json&gene=PSCA&cohort=BLCA&protocol=RSEM&page=1&page_size=250&sort_by=cohort');
+  //let x={hello:'world'}
   res.end(JSON.stringify(x,null,3));
 });
 
 server.listen(port);
+console.log(`server listening to port ${port} since ${Date()}`)
